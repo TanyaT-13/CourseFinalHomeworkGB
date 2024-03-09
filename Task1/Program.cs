4fbc70381347bd9525f5e2ef05186b12b61341f6
Tanya Tyurkina <bts13@yandex.ru>
2024-03-09 23:15:01 +0300

    Решили задачу 1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void LineOfDigits(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    Console.Write($"{M} ");
+    LineOfDigits(M + 1, N);
+    
+}
+Console.WriteLine("Введите первое число промежутка: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число промежутка: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input rows: ");
+
+LineOfDigits(M, N);
