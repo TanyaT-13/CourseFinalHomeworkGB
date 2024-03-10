c97c68a696a47bf455685b014257c3546f510a93
Tanya Tyurkina <bts13@yandex.ru>
2024-03-10 13:20:37 +0300

    Изменили название функции на более понятное

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,21 +2,21 @@
 // которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию, не использовать циклы.
 
-void LineOfDigits(int M, int N)
+void ShowLineOfDigits(int M, int N)
 {
     if (N < M)
     {
         return;
     }
     Console.Write($"{M} ");
-    LineOfDigits(M + 1, N);
+    ShowLineOfDigits(M + 1, N);
     
 }
-LineOfDigits(2, 15);
+ShowLineOfDigits(2, 15);
 
 // А можно вместо последней строки сделать ввод чисел промежутка с консоли:
 // Console.WriteLine("Введите первое число промежутка: ");
 // int M = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine("Введите первое число промежутка: ");
 // int N = Convert.ToInt32(Console.ReadLine());
-// LineOfDigits(M, N);
+// ShowLineOfDigits(M, N);
--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b65717ee73237431c21bdba4c6c18031062295df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117e8f006803fc27f6ab40edd272ee6d5ec90672")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
